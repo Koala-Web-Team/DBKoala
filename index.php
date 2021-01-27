39bb4202b34acc4584e9fc794ed57ade3225d4fd
RamajAlaa5 <ramojaalaa15@gmail.com>
2021-01-27 14:31:05 +0200

    Raw and selectRaw Query Task

--- index.php
+++ index.php
@@ -5,7 +5,8 @@ require_once("QueryBuilder/Table.php");
 $user = new Table('users');
 
 $result = $user->find('1');
-
+	//	$result = $user->selectRaw('count(*) as user_count, name');
+	//	$result=$user->raw(' count(*) as dept_total');
 print_r($result->first_name);
 
 
